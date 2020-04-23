22b424d12ff368def6c46b0803a8812e844db4aa
Yasassri Purnajith <yasassri89@gmail.com>
2020-04-23 17:23:33 +0530

    Rabbit MQ sample

---
+++ HubspotTest.RabbitMQ.Reciever/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace HubspotTest.RabbitMQ.Reciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                Port = 5672,
+                RequestedConnectionTimeout = new TimeSpan(0, 0, 0, 0, 3000) // milliseconds
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "testqueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "testqueue",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ HubspotTest.RabbitMQ.Sender/Program.cs
@@ -0,0 +1,64 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubspotTest.RabbitMQ.Sender
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            const string queueName = "testqueue";
+
+            try
+            {
+                var connectionFactory = new ConnectionFactory()
+                {
+                    HostName = "localhost",
+                    UserName = "guest",
+                    Password = "guest",
+                    Port = 5672,
+                    RequestedConnectionTimeout = new TimeSpan(0, 0, 0, 0, 3000) // milliseconds
+                };
+
+                using (var rabbitConnection = connectionFactory.CreateConnection())
+                {
+                    using (var channel = rabbitConnection.CreateModel())
+                    {
+                        // Declaring a queue is idempotent 
+                        channel.QueueDeclare(
+                            queue: queueName,
+                            durable: false,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+
+                        while (true)
+                        {
+                            string body = $"A nice random message: {DateTime.Now.Ticks}";
+                            channel.BasicPublish(
+                                exchange: string.Empty,
+                                routingKey: queueName,
+                                basicProperties: null,
+                                body: Encoding.UTF8.GetBytes(body));
+
+                            Console.WriteLine("Message sent" + body);
+                            await Task.Delay(500);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.WriteLine("End");
+            Console.Read();
+        
+        }
+    }
+}
